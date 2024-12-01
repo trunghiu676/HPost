08fa965ac421bdf68f2077410e9a4db424559cbd
Hoàng Trung Hỉu <trunghiu676@gmail.com>
2024-12-01 22:13:06 +0700

    Thêm identity ui, đăng nhập - đăng ký, đăng nhập gg, send mail, sử dụng admin template, add libman

---
+++ Data/RoleName.cs
@@ -0,0 +1,10 @@
+namespace App.Data
+{
+    //Class để khởi tạo các role, seed data trong dbmanage trong area database
+    public class RoleName 
+    {
+        public const string Administrator = "Administrator";
+        public const string Editor = "Editor";
+        public const string Member = "Member";
+    }
+}
